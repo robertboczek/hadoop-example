1f3492b6aea08393f28a1ab09f81d6a48fe821f5
Robert Boczek <robert.boczek@gmail.com>
2013-10-27 14:43:47 -0700

    Working example

---
+++ runJob.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Find more:
+# http://www.michael-noll.com/tutorials/running-hadoop-on-ubuntu-linux-single-node-cluster/
+
+bin/hadoop jar /tmp/wordcount.jar /user/hadoop-user/input.txt /user/hadoop-user/output
+
+
